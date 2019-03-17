b6dbdbce1072cae80068f36f7c37a3927e5858e4
Naveen Mullapudi <naveen31saurav@gmail.com>
2019-03-17 19:39:17 +0530

    Second commit

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -3,12 +3,12 @@ import org.testng.annotations.Test;
 public class NewTest {
   @Test
   public void f3() {
-	  System.out.println("I am in testng function");
+	  System.out.println("I am in testng function in gitttt1");
   }
   
   @Test
   public void f2() {
-	  System.out.println("I am in testng function2");
+	  System.out.println("I am in testng function2 Gitttttttt22");
   }
   
   
