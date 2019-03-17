52a1741da5305880bd4faf699a999db5c277cef4
Naveen Mullapudi <naveen31saurav@gmail.com>
2019-03-17 19:26:52 +0530

    First commit

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f3() {
+	  System.out.println("I am in testng function");
+  }
+  
+  @Test
+  public void f2() {
+	  System.out.println("I am in testng function2");
+  }
+  
+  
+}
---
+++ src/test/java/NewTest2.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class NewTest2 {
+  @Test
+  public void f() {
+	  
+	  System.out.println("I am in testng class2 ");
+  }
+}
